51ea25d8cae432850f79956e419580a348d38af2
j-rivs <jrivs15@terpmail.umd.edu>
2022-12-22 16:13:56 -0600

    Add code and pass tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,20 @@
-const reverseString = function() {
+/*
+    Structure
+    storage variable
+    get string length
+    remove last character in string
+        confirm string methods
+        use array if necessary
+    add last character removed to temp variable
+    return temp variable
+*/
 
+const reverseString = function(reverse) {
+    let holder = '';
+    for (let i = reverse.length; i > 0; i--) {
+        holder = holder.concat(reverse.slice((i-1), i));
+    }
+    return holder;
 };
 
 // Do not edit below this line
