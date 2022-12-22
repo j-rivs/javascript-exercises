f8fe06843addae2a7e2f3f144fabe30438c86558
j-rivs <jrivs15@terpmail.umd.edu>
2022-12-22 00:43:23 -0600

    Added code and passed tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(rep, count) {
+    let message = '';
+    if (count < 0) {
+        return 'ERROR';
+    }
+    for (let i = 0; i < count; i++) {
+        message = message.concat(rep);
+    }
+    return message;
 };
 
 // Do not edit below this line
