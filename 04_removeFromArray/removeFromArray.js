df96225181e18d382dbc7810aeeb7f4f3be018ff
j-rivs <jrivs15@terpmail.umd.edu>
2022-12-23 13:15:06 -0600

    Add code and pass tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
 const removeFromArray = function() {
+    let numbs = arguments[0];
 
+    for (let i = 0; i < arguments.length;i++) {
+            for (let n = 0; n < numbs.length; n++) {
+                if (numbs[n] === arguments[i]) {
+                    numbs.splice(n, 1);
+                }
+            }
+    }
+
+    return numbs;
 };
 
 // Do not edit below this line
