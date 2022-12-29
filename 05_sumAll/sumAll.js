d2c24f284ad341311235b470c1390a40f6e81259
j-rivs <jrivs15@terpmail.umd.edu>
2022-12-28 18:33:44 -0600

    Add code and pass test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,42 @@
 const sumAll = function() {
+    let large = arguments[0];
+    let small = arguments[1];
+    if (arguments[0] < arguments[1]) {
+        large = arguments[1];
+        small = arguments[0];
+    }
 
+    console.log(large);
+    console.log(small);
+
+    if ((typeof large) !== 'number'|| (typeof small) !== 'number') {
+        return 'ERROR';
+    }
+    if (large < 0 || small < 0) {
+        return 'ERROR';
+    }
+
+    let sum = 0;
+
+    for (let i = small; i <= large; i++) {
+        sum = sum + i;
+    }
+
+    return sum;
 };
+
+/*
+Check arguments for 'number'
+    return Error if not
+Check which argument is bigger
+    Assign variable to higher and lower arg
+
+Create return variable
+Loop between variables
+    add / += into sum variable
+
+
+*/
 
 // Do not edit below this line
 module.exports = sumAll;
