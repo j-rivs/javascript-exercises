3939e53ee62bf880eab400c03d9108cdfe64e690
j-rivs <jrivs15@terpmail.umd.edu>
2022-12-29 22:04:05 -0600

    Add code for 4/6 tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
 const leapYears = function() {
-
+    let rem = arguments[0] % 4;
+    if (rem ===0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
