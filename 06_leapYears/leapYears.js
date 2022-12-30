bf200082a08059ca1da11cc8947d3560f67ee36d
j-rivs <jrivs15@terpmail.umd.edu>
2022-12-29 22:48:59 -0600

    Add 2nd temp var and fix nested if logic

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,7 +1,12 @@
 const leapYears = function() {
+    let cent = arguments[0];
     let rem = arguments[0] % 4;
-    if (rem ===0) {
-        return true;
+    if (rem === 0) {
+        if (cent % 100 === 0 && cent % 400 !== 0) {
+            return false;
+        } else {
+            return true;
+        }
     } else {
         return false;
     }
