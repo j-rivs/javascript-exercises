1d6996f88d50041b7638c0e38a1cf18ea5eb2952
j-rivs <jrivs15@terpmail.umd.edu>
2022-12-31 00:33:07 -0600

    Finish algorithms and remove test skips

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,24 @@
 const convertToCelsius = function() {
+
+  let text = (arguments[0] - 32) * 5 / 9;
+  if (text % 1 === 0) {
+    text = Number(text.toFixed(0));
+  } else {
+    text = Number(text.toFixed(1));
+  }
+
+  return text;
 };
 
 const convertToFahrenheit = function() {
+  let text = (arguments[0] * (9/5)) + 32;
+  if (text % 1 === 0) {
+    text = Number(text.toFixed(0));
+  } else {
+    text = Number(text.toFixed(1));
+  }
+
+  return text;
 };
 
 // Do not edit below this line
